087c68beb0cc365f911ec86163f08a6b3dda949a
Gustavo Jetson <gustavo041196jp@gmail.com>
2023-05-14 11:22:02 -0600

    Se agregan mas comandos a numbers

--- 04_number.py
+++ 04_number.py
@@ -1,3 +1,17 @@
+#NUMBERS
 lives = 3
 print(lives)
 print(type(lives))
+age   = 12
+budget = 100
+temperature = 12.12
+print(type(temperature))
+lives = 12+15 
+print(lives-2)
+lives+= 1
+print(lives)
+
+number = 4500000000000000000000000000.1
+print(number)
+number1 = 0.000000023
+print(number1)
