87e22e3652426bfdbef43bf2a41e98ec4a84e0c9
Gustavo Jetson <gustavo041196jp@gmail.com>
2023-05-13 23:55:16 -0600

    Terminamos codigo de tipos de datos 02_types

--- 02_types.py
+++ 02_types.py
@@ -2,3 +2,18 @@
 my_name = "Gustavo"
 my_name = 'Gustavo Tovar'
 print("my name =>",my_name)
+print(type(my_name))
+
+# int
+my_age = 12
+print('my_age =>', my_age)
+print(type(my_age))
+
+# boolean
+is_single = False
+print('is_single =>', is_single)
+print(type(is_single))
+
+# inputs
+my_age = input('Cual es tu edad? ')
+print('my_age =>', my_age)
