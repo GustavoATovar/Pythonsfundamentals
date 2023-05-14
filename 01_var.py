df93ec33ae4628c387cd5918f6fbef9f264225b8
Gustavo Jetson <gustavo041196jp@gmail.com>
2023-05-13 22:04:40 -0600

    Usando print e input en python

--- 01_var.py
+++ 01_var.py
@@ -1,2 +1,16 @@
-sum = 1+2
-print(sum)
+print("Hola,vars")
+#Esta es una variable 
+my_name= "Gustavo Tovar"
+print (my_name)
+#Esta es una variable con un numero
+my_age = 12
+print(my_age) 
+
+my_name= "Gustavo"
+print("Aqui hice un cambio ", my_name)
+
+#Input te permite ingresar un valor
+my_name= input("Cual es tu nombre?")
+print("Usando input",my_name)
+my_age= input("Cual es tu edad?")
+print("Mi edad es", my_age)
