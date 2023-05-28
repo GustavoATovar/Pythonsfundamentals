a71f79fdd90527183b84365774f19fce97fe456e
Gustavo Jetson <gustavo041196jp@gmail.com>
2023-05-28 13:48:18 -0600

    Se agrega codigo de dictionary

---
+++ 08_dictionary.py
@@ -0,0 +1,14 @@
+#Learn how to use a dictionary
+
+my_dict = {} 
+print(type(my_dict))
+
+my_dict = {
+        'avion': "avion 320"
+        'Name': "Gustavo"
+        'Lastname': "Tovar"
+}
+
+print(my_dict('Name'))
+print(len(my_dict))
+print(my_dict.get('Name))
