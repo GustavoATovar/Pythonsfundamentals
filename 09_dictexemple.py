d5c016a334c8574dd17399cbca58cdb383ae4c76
Gustavo Jetson <gustavo041196jp@gmail.com>
2023-05-29 21:33:16 -0600

    Se agregan ejercicios de platzi

--- 09_dictexemple.py
+++ 09_dictexemple.py
@@ -1,8 +1,39 @@
 #Exemple of a dictionary
 
-persona = {
+person = {
         'name': 'Gustavo',
         'lastname': 'Tovar',
-        'langs': ['python','javascript'],
         'age': 26
         }
+
+person['twiter']= "@nicobytes"
+print(person)
+person['name']= "Carlos"
+print(person)
+del person['age']
+print(person)
+print('Keys')
+print(person.keys())
+print('Values')
+print(person.values())
+
+"""
+print(person)
+person['name'] = 'Santi'
+print(person)
+person['langs'].append('rust')
+print(person)
+del person['lastname']
+print(person)
+person.pop('age')
+print(person)
+
+print('items')
+print(person.items())
+
+print('keys')
+print(person.keys())
+
+print('values')
+print(person.values())
+"""
