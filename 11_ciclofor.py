919a5b44bbc502fb7277e33917fb88a4eaed8ccf
Gustavo Jetson <gustavo041196jp@gmail.com>
2023-05-30 21:04:45 -0600

    Se agrega codigo 11,con ejemplos de ciclo for

---
+++ 11_ciclofor.py
@@ -0,0 +1,19 @@
+# CICLO FOR 
+"""
+for element in  range(5,20):
+ print(element)
+"""
+my_list = [25,43,26]
+for element in my_list:
+    print(element)
+my_tuple = ('tavo','esme','charly')
+for element in my_tuple:
+    print(element)
+dictionary = {
+        'name': 'tavo',
+        'age': 26
+        } 
+for element in dictionary:
+    print(dictionary[element])
+for atributo in dictionary:
+    print(atributo,'==>',dictionary[atributo])
